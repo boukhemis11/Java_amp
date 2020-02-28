a931e0b73c6ddee37076ca350ce2f6af3cc94eab
Boukhemis Dib <boukhemis2011@gmail.com>
2020-02-28 12:34:02 +0100

    1st commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,27 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		int var1 = 12;
+		byte var2 = 99;
+		short var3 = -11325;
+		int var4 = 1533325;
+		long var5 = 17684L;
+		float var6 = 1.6F;
+		double var7 = 1.8;
+		boolean var8 = true;
+		char var9 = 'c';
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
+		System.out.println(var7);
+		System.out.println(var8);
+		System.out.println(var9);
+	}
+
+}
