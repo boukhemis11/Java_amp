0198f509dbe70fc37bdb2d79160b6efc6861e4ee
Boukhemis Dib <boukhemis2011@gmail.com>
2020-03-02 10:33:25 +0100

    Fix bug

--- src/fr/algorithmie/FirstLast.java
+++ src/fr/algorithmie/FirstLast.java
@@ -4,15 +4,15 @@ public class FirstLast {
 
 	public static void main(String[] args) {
 		int[] array = {4,3,6,1,4};
-		boolean cont;
 		
-		if(array.length> 1 && array[0] == array[array.length-1 ] ) {
-			cont = true;
+		/*On calcule une valeur booléenne qui contrôle le tableau de la sorte :
+		 	elle vaut true si le tableau est de longueur strictement supérieure à 1 et que le
+			premier et le dernier élément du tableau ont la même valeur
+			elle vaut false dans les autres cas
+		 */
+		
+		boolean cont = ((array.length> 1 )&& (array[0] == array[array.length-1 ] ));
 			System.out.println("controle est " +cont);
-		}else {
-			cont = false;
-			System.out.println("controle est " +cont);
-		}
 
 	}
 
