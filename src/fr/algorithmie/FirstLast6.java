0add179c4e28ab65307fc6e675292c2531f43375
Boukhemis Dib <boukhemis2011@gmail.com>
2020-03-02 10:18:39 +0100

    EXERCICE FIRSTLAST6

---
+++ src/fr/algorithmie/FirstLast6.java
@@ -0,0 +1,23 @@
+package fr.algorithmie;
+
+public class FirstLast6 {
+
+	public static void main(String[] args) {
+		int[] array = {6,33,45,1,-85,96,5};
+		boolean cont;
+		
+		/*On calcule une valeur booléenne qui contrôle le tableau de la sorte : 
+		    elle vaut true si le tableau a au moins 1 élément et si le premier élément ou le dernier élément vaut 6.
+		    elle vaut false dans les autres cas */
+		 
+		
+		if(array.length> 0 && array[0] == 6 || array[array.length-1 ] == 6) {
+			cont = true;
+			System.out.println("controle est " +cont);
+		}else {
+			cont = false;
+			System.out.println("controle est " +cont);
+		}
+	}
+
+}
